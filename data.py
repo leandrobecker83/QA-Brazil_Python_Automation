7125daf57e258c4527932eca46edea036757ed45
leandrobecker83 <leandrobecker83@hotmail.com>
2025-11-28 13:55:40 -0300

    Projeto sprint 7

--- data.py
+++ data.py
@@ -1,1 +1,8 @@
 # Arquivo com constantes
+URBAN_ROUTES_URL = 'https://cnt-5d00d54f-24d7-4aec-a295-c0c369e3610a.containerhub.tripleten-services.com?lng=pt'
+ADDRESS_FROM = 	'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Pare no bar de sucos'
